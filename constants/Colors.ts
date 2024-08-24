f1dfba0dc6bf9316b046df1ab54bdbb0b7e74387
evakhromeeva <vakhromeevae@gmail.com>
2024-08-24 16:06:40 +0300

    add questionnaire

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,8 +3,22 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorBeLight = '#336021';
-const tintColorBeDark = '#fff';
+export const tintColorBeLight = '#336021';
+export const tintColorBeDark = '#fff';
+
+export const kLightYellow = "#FFF9ECFF";
+export const kLightYellow2 = "#FFE4C7FF";
+export const kDarkYellow = "#F9BE7CFF";
+export const kPalePink = "#FED4D6FF";
+export const kRed = "#FFE46472";
+export const kLavender = "#D5E4FEFF";
+export const kBlue = "#6488E4FF";
+export const kLightGreen = "#D9E6DCFF";
+export const kBlueGreen = "#309397FF";
+export const kDarkGreen = "#336021FF";
+export const kGreen = "#9AC588FF";
+export const kDarkBlue = "#0D253FFF";
+
 
 export const Colors = {
   light: {
--- constants/GraphColors.ts
+++ constants/GraphColors.ts
@@ -3,22 +3,23 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
+import { kLightYellow, kLightGreen, kPalePink, kLavender, kDarkBlue, kBlue } from "./Colors";
+
 export const GraphColors = {
   light: {
-    proteins: "red",
-    fats: "yellow",
-    carbohydrates: "white",
-    fiber: "green",
-    water: "blue",
+    proteins: kPalePink,
+    fats: kLightYellow,
+    carbohydrates: kLavender,
+    fiber: kLightGreen,
+    water: kBlue,
     legend: "black"
-
   },
   dark: {
-    proteins: "red",
-    fats: "yellow",
-    carbohydrates: "white",
-    fiber: "green",
-    water: "blue",
+    proteins: kPalePink,
+    fats: kLightYellow,
+    carbohydrates: kLavender,
+    fiber: kLightGreen,
+    water: kBlue,
     legend: "black"
   }
 };
